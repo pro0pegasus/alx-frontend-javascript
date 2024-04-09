52c3de6302ff35d9ca6fc04a7a4599af697435d2
Yassine Borak <yassineborak@gmail.com>
2024-04-09 02:02:38 +0000

    quick fix

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,4 +1,4 @@
-testexport default function createReportObject(employeesList) {
+export default function createReportObject(employeesList) {
   return {
     allEmployees: {
       ...employeesList,
