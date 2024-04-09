006fceaa1997684eb53ed3392f941b4b31b5e336
Yassine Borak <yassineborak@gmail.com>
2024-04-09 02:06:34 +0000

    quick fix

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,3 @@
 export default function createIteratorObject(report) {
-  let allEmployees = [];
-  for (const item of Object.values(report.allEmployees)) {
-    allEmployees = [
-      ...allEmployees,
-      ...item,
-    ];
-  }
-  return allEmployees;
+  return Object.values(report.allEmployees).flat();
 }
