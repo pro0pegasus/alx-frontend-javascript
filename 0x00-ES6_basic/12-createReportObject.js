b52cc6cafc30197b3172af71b08586ee4e9cf949
Yassine Borak <yassineborak@gmail.com>
2024-04-09 01:49:18 +0000

    ES6 basic

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+testexport default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
+  };
+}
