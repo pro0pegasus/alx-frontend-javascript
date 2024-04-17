837362af9a493a8e961c4bb508e74758dc78018f
Yassine Borak <yassineborak@gmail.com>
2024-04-17 00:19:18 +0000

    quick fix

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(arr, city) {
-  return arr.filter((student) => student.location === city);
+export default function getStudentsByLocation(studentArray, city) {
+  return studentArray.filter((studentObj) => studentObj.location === city);
 }
--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,3 @@
-
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
     throw new Error('Position outside range');
